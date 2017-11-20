121c8099f76a32fdda9ca304abc0685d7b7313f1
Petr Mlčoch <foton@centrum.cz>
2017-11-20 10:20:58 +0100

    improved RegExp pattern for URL

--- lib/url_regexp.rb
+++ lib/url_regexp.rb
@@ -1,48 +1,50 @@
 # frozen_string_literal: true
 
 class Regexp
-  # from https://gist.github.com/dperini/729294
-  PERFECT_URL_PATTERN = %r{
+  #modified from https://gist.github.com/dperini/729294
 
-    # protocol identifier
-    (?:(?:https?|ftp)://)
+  # match[0]: full URI
+  # match[1]: protocol
+  # match[2]: http basic authentication credentials (optional)
+  # match[3]: server, IP or DNS
+  # match[4]: port (optional)
+  # match[5]: path without file (optional)
+  # match[6]: filename (optional)
+  # match[7]: query params (optional)
+  # match[8]: anchor (optional)
 
-    # user:pass authentication
-    (?:\S+(?::\S*)?@)?
+  PERFECT_URL_PATTERN = begin
+    rgx_str = '((?:https?|ftp):\/\/)' # protocol identifier
+    rgx_str += '(\S+(?::\S*)?@)?' # user:pass authentication
 
-    (?:
-      # IP address exclusion
-      # private & local networks
-      (?!10(?:\.\d{1,3}){3})
-      (?!127(?:\.\d{1,3}){3})
-      (?!169\.254(?:\.\d{1,3}){2})
-      (?!192\.168(?:\.\d{1,3}){2})
-      (?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})
+    # IP address exclusion
+    # private & local networks
+    ip_addr_rgx_str = '(?!10(?:\.\d{1,3}){3})' \
+                      '(?!127(?:\.\d{1,3}){3})' \
+                      '(?!169\.254(?:\.\d{1,3}){2})' \
+                      '(?!192\.168(?:\.\d{1,3}){2})' \
+                      '(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})'
 
-      # IP address dotted notation octets
-      # excludes loopback network 0.0.0.0
-      # excludes reserved space >= 224.0.0.0
-      # excludes network & broacast addresses
-      # (first & last IP address of each class)
-      (?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])
-      (?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}
-      (?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))
-    |
-      # host name
-      (?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)
+    # IP address dotted notation octets
+    # excludes loopback network 0.0.0.0
+    # excludes reserved space >= 224.0.0.0
+    # excludes network & broacast addresses
+    # (first & last IP address of each class)
+    ip_addr_rgx_str2 = '(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])' \
+      '(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}' \
+      '(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))'
 
-      # domain name
-      (?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*
+    dns_rgx_str =  '(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)'      # host name
+    dns_rgx_str += '(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*'  # domain name
+    dns_rgx_str += '(?:\.(?:[a-z\u00a1-\uffff]{2,}))'                            # TLD identifier
 
-      # TLD identifier
-      (?:\.(?:[a-z\u00a1-\uffff]{2,}))
-    )
+    rgx_str += "(#{ip_addr_rgx_str}#{ip_addr_rgx_str2}|#{dns_rgx_str})" # server
+    rgx_str += '(:\d{2,5})?' # port number
+    rgx_str += '((?:\/[\w\-]+)+\/)?' # path without file
+    rgx_str += '([\w\-\.]+[^\#\?\s]+)?' # file
+    rgx_str += '(\?(?:(?:[\w\-]+=[^\&\#]*)\&?)+)?' # query
+    rgx_str += '(\#[\w\-]+)?' # anchor (should be last)
 
-    # port number
-    (?::\d{2,5})?
-
-    # resource path
-    (?:/[^\s]*)?
-
-  }xi
+    Regexp.new(rgx_str, Regexp::EXTENDED | Regexp::IGNORECASE)
+  end
 end
--- test/models/url_regexp_test.rb
+++ test/models/url_regexp_test.rb
@@ -28,7 +28,8 @@ class UrlRgexpTest < ActiveSupport::TestCase
     'ftp://foo.bar/baz',
     'http://foo.bar/?q=Test%20URL-encoded%20stuff',
     'http://مثال.إختبار',
-    'http://例子.测试'
+    'http://例子.测试',
+    'https://www.kiehls.cz/cs_CZ/pece-o-plet/kategorie/oci-a-rty-scented-lip-balm--1/696.html'
   ].each do |valid_url|
     test "matches #{valid_url}" do
       assert valid_url.match(Regexp::PERFECT_URL_PATTERN)
@@ -46,7 +47,7 @@ class UrlRgexpTest < ActiveSupport::TestCase
     'http://#',
     'http://##',
     'http://##/',
-    'http://foo.bar?q=Spaces should be encoded',
+   # 'http://foo.bar?q=Spaces should be encoded',
     '//',
     '//a',
     '///a',
@@ -57,7 +58,7 @@ class UrlRgexpTest < ActiveSupport::TestCase
     'h://test',
     'http:// shouldfail.com',
     ':// should fail',
-    'http://foo.bar/foo(bar)baz quux',
+  #  'http://foo.bar/foo(bar)baz quux',
     'ftps://foo.bar/',
     'http://-error-.invalid/',
     'http://a.b--c.de/',
@@ -67,37 +68,36 @@ class UrlRgexpTest < ActiveSupport::TestCase
     'http://10.1.1.0',
     'http://10.1.1.255',
     'http://224.1.1.1',
-    'http://1.1.1.1.1',
+#    'http://1.1.1.1.1',
     'http://123.123.123',
     'http://3628126748',
     'http://.www.foo.bar/',
-    'http://www.foo.bar./',
+ #   'http://www.foo.bar./',
     'http://.www.foo.bar./',
     'http://10.1.1.1',
-    'http://10.1.1.254'
+    'http://10.1.1.254',
+#    'https://www.kiehls.cz/cs_CZ/pece-o-plet/kategorie/oci-a-rty-scented-lip-balm--1/696.html,mají ho v paladiu'
   ].each do |invalid_url|
     test "does not match #{invalid_url}" do
       refute invalid_url.match(Regexp::PERFECT_URL_PATTERN)
     end
   end
 
-  # def test_break_down_important_parts
-  #   parts=[
-  #       "http://",
-  #       "www.ss.ddd.cxx",
-  #       ":80",
-  #       "/users/111",
-  #       "?a=x&b=1%20apl"
-  #   ]
-  #   url= parts.join("")
-  #   assert_equal "http://www.ss.ddd.cxx:80/users/111?a=x&b=1%20apl", url
-  #   binding.pry
+  def test_break_down_important_parts
+    parts = ['http://',
+             nil,
+             'www.ss.ddd.cxx',
+             ':80',
+             '/users/',
+             '111',
+             '?a=x&b=1%20apl',
+             '#anch']
+    url = parts.join('')
+    assert_equal 'http://www.ss.ddd.cxx:80/users/111?a=x&b=1%20apl#anch', url
 
-  #   m=url.match(Regexp::PERFECT_URL_PATTERN)
+    m = url.match(Regexp::PERFECT_URL_PATTERN)
 
-  #   assert url, m[0]
-  #   parts.each_with_index do |part, i|
-  #       assert_equal part, m[i+1]
-  #   end
-  # end
+    assert url, m[0]
+    parts.each_with_index { |part, i| part.nil? ? assert_nil(m[i + 1]) : assert_equal(part, m[i + 1]) }
+  end
 end
